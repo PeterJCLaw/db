8d68b44896ec9c847adc649602dcd9a7e81df752
Peter Law <PeterJCLaw@gmail.com>
2011-06-16 20:36:00 +0100

    Make the constructor protected.
    This allows child classes to manage their own instances, without
    restrricting them to the singleton pattern without having to
    re-implement the constructor.

--- db.class.php
+++ db.class.php
@@ -35,8 +35,8 @@ class db extends mysqli {
 	const ERR_CONNECT_ERROR = 6002;
 	const ERR_CLONE = 6003;
 
-	// A private constructor; prevents direct creation of object
-	private function __construct($server = "localhost", $username = "", $password = "", $schema = ""){
+	// A protected constructor; prevents direct creation of object
+	protected function __construct($server = "localhost", $username = "", $password = "", $schema = ""){
 		$this->database = $schema;
 
 		// Prevents mysql sock warnings. I prefer to throw exception as below.
